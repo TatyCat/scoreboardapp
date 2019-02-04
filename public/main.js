93c5fac147774cea43064b6a492deb27b418cde1
TatyCat <williams.tatyanna@gmail.com>
2019-02-04 18:09:45 -0500

    Completed Exploer Mode

--- public/main.js
+++ public/main.js
@@ -1,7 +1,60 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+//  OBJ: A user should be able to update a score and the teams playing.
+
+// SCORE-----------------------------
+// set a starting score
+let score1 = 0
+let score2 = 0
+
+let updateScore1Add = () =>{
+  score1++
+  document.querySelector(".team1Score").textContent = score1 
 }
 
-document.addEventListener('DOMContentLoaded', main)
+let updateScore2Add = () =>{
+  score2++
+  document.querySelector(".team2Score").textContent = score2 
+}
+
+let updateScore1Sub = () =>{
+  score1--
+  document.querySelector(".team1Score").textContent = score1 
+}
+
+let updateScore2Sub = () =>{
+  score2--
+  document.querySelector(".team2Score").textContent = score2 
+}
+
+
+// Select the class for both scores
+// Update the score to reflect the new global score
+
+// if  add addButton, increment
+document.querySelector('.team-1-add-1-button').addEventListener('click', updateScore1Add)
+
+document.querySelector('.team-2-add-1-button').addEventListener('click', updateScore2Add)
+
+// if  add subtract Button, decrement
+document.querySelector('.team-1-subtract-1-button').addEventListener('click', updateScore1Sub)
+
+document.querySelector('.team-2-subtract-1-button').addEventListener('click', updateScore2Sub)
+
+
+// TEAMS-----------------------------
+//if update button is pressed, capture input.
+//display input for Team Name
+
+changeTeamName1 = () =>{
+  document.querySelector('.team1 h2').textContent = document.querySelector('.team-name input').value
+}
+
+changeTeamName2 = () => {
+   document.querySelector('.team2 h2').textContent = document.querySelector('.team2 input').value
+}
+
+document.querySelector(".update-team-1-name").addEventListener('click', changeTeamName1)
+
+document.querySelector(".team2 button").addEventListener('click', changeTeamName2);
+
+
+
