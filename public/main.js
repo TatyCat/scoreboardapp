fb3829c933ad6f4c391ac29c755db1eb7737b616
TatyCat <williams.tatyanna@gmail.com>
2019-02-06 13:53:41 -0500

    Completed ScoreBoard Project App v.1

--- public/main.js
+++ public/main.js
@@ -11,18 +11,25 @@ let team1ScoreText = document.querySelector(".team1Score")
 let team2ScoreText = document.querySelector(".team2Score")
 let team1TeamNameText = document.querySelector('.team1 h2')
 let team2TeamNameText = document.querySelector('.team2 h2')
+const countdownText = document.querySelector("#period-timer span")
+let timerStart
+
+/*
+const countdownText = document.querySelector("#period-timer span")
+
+const getCountDownText = () => {
+  return document.querySelector('#period-timer span')
+}
+*/
 
 const updateScore1Add = () =>{
-  if (score1< 2){
+  if (score1< 21){
     score1++
     getRandomColor(score1)
     team1ScoreText.textContent = score1 
 
-    if (score1 === 2){
-      team1ScoreText.classList.add("winningTeam")
-      team1TeamNameText.textContent = `${team1TeamNameText.textContent} YOU WIN!!!!`
-      team1TeamNameText.classList.add("winningTeam")
-      disableAllButtons()
+    if (score1 === 21){
+      winner(1)
     } 
   }
 }
@@ -33,10 +40,12 @@ const updateScore2Add = () =>{
     getRandomColor(score2)
     team2ScoreText.textContent = score2
     if (score2 === 21){
-      team2ScoreText.classList.add("winningTeam")
-      team2TeamNameText.textContent = `${team2TeamNameText.textContent} YOU WIN!!!!`
-      team2TeamNameText.classList.add("winningTeam")
-      disableAllButtons()
+      winner(2)
+
+      // team2ScoreText.classList.add("winningTeam")
+      // team2TeamNameText.textContent = `${team2TeamNameText.textContent} YOU WIN!!!!`
+      // team2TeamNameText.classList.add("winningTeam")
+      // gameOverButtonsGone()
     }
   }
 }
@@ -86,16 +95,48 @@ document.querySelector(".update-team-1-name").addEventListener('click', changeTe
 
 document.querySelector(".team2 button").addEventListener('click', changeTeamName2)
 
+// WINNER FUNCTION-----------------------------
+const winner = (teamNumber) => {
+  let scoreText
+  let teamNameText
+  
+  clearInterval(timerStart)
+
+  if (teamNumber === 1) {
+    scoreText = team1ScoreText
+    teamNameText = team1TeamNameText
+  } else {
+    scoreText = team2ScoreText
+    teamNameText = team2TeamNameText
+  }
+
+  scoreText.classList.add("winningTeam")
+  teamNameText.textContent = `${teamNameText.textContent} YOU WIN!!!!`
+  teamNameText.classList.add("winningTeam")
+  gameOverButtonsGone()
+  document.querySelector("#gameTimeButton").disabled = true
+  document.querySelector(".resetButton").style.visibility = "visible"
+
+  // team1ScoreText.classList.add("winningTeam")
+  // team1TeamNameText.textContent = `${team1TeamNameText.textContent} YOU WIN!!!!`
+  // team1TeamNameText.classList.add("winningTeam")
+  // gameOverButtonsGone()
+  // document.querySelector("#gameTimeButton").disabled = true
+  // document.querySelector(".resetButton").style.visibility = "visible"
+}
+
+
 // COUNTDOWN TIMER-----------------------------
 //45m each half/quarter
-const countdownText = document.querySelector("#period-timer span")
 
 const oneQuarterTimeFrame = () => {
   let initialTime = Date.now();
   let periodCount = 0
 
   function checkTime(){
+
     let timeDifference = Date.now() - initialTime;
+
     let formatted = convertTime(timeDifference);
   }
 
@@ -104,28 +145,22 @@ const oneQuarterTimeFrame = () => {
     let minutes = Math.floor(totalSeconds/60);
     let seconds = totalSeconds - minutes * 60;
  
-    if (minutes === 45 || minutes === 90 || seconds === 3){
+    if (minutes === 4 || minutes === 9){
       // QUARTER PERIOD COUNT-----------------------------
       // 2 periods per game 
       if (periodCount < 2){
         document.querySelector("#period").textContent = periodCount++
       }
 
-      if(seconds === 3){
+      if(minutes === 90){
         clearInterval(timerStart)
         //if team 1 || team 2 do not have winning team class-declare winner....else/nothing
         if (!team1ScoreText.classList.contains("winningTeam") && !team2ScoreText.classList.contains("winningTeam"))
         {  
           if (score1 > score2){
-            team1ScoreText.classList.add("winningTeam")
-            team1TeamNameText.textContent = `${team1TeamNameText.textContent} YOU WIN!!!!`
-            team1TeamNameText.classList.add("winningTeam")
-            disableAllButtons()
+            winner(1)
           }else if (score2 > score1){
-            team2ScoreText.classList.add("winningTeam")
-            team2TeamNameText.textContent = `${team2TeamNameText.textContent} YOU WIN!!!!`
-            team2TeamNameText.classList.add("winningTeam")
-            disableAllButtons()
+            winner(2)
           }
         }
       }
@@ -137,9 +172,11 @@ const oneQuarterTimeFrame = () => {
     if(seconds.toString().length === 1){
       seconds = "0"+seconds
     }
+
     countdownText.textContent = minutes + ':' + seconds;
   }
-  let timerStart = window.setInterval(checkTime, 100);
+  timerStart = window.setInterval(checkTime, 100)
+  
 }
 
 const countdownTimer = () => {
@@ -151,13 +188,12 @@ document.querySelector("#gameTimeButton").addEventListener('click', countdownTim
 
 
 // DISABLE BUTTON-----------------------------
-let allButtons = document.querySelectorAll(".disabledButtonGameOver")
-
-const disableAllButtons = () =>{
-  allButtons.disabled = true
-  document.querySelector(".resetButton").style.visibility = "visible"
+let gameButtonsSection = document.querySelector(".update-score")
+const gameOverButtonsGone = () =>{
+  gameButtonsSection.style.visibility = "hidden"
 }
 
+// RESET-------------------------------------
   const resetEverything = () =>{
     //Reset Scores & Display
     score1 = 0
@@ -166,8 +202,10 @@ const disableAllButtons = () =>{
     team2ScoreText.textContent = score2
 
     //Reset Buttons 
-    allButtons.disabled = false
+    document.querySelector(".resetButton").style.visibility = "hidden"
+    gameButtonsSection.style.visibility = "visible"
     document.querySelector("#gameTimeButton").style.visibility = "visible"
+    document.querySelector("#gameTimeButton").disabled = false
 
 
     //Reset Background
@@ -187,7 +225,7 @@ const disableAllButtons = () =>{
     document.querySelector("#period").textContent = 0
 
     //Reset Timer
-    document.querySelector("#period-timer").textContent = "00:00"
+    countdownText.textContent = "00:00"
 
     //Reset the Reset Button
     document.querySelector(".resetButton").style.visibility = "hidden"
@@ -195,7 +233,7 @@ const disableAllButtons = () =>{
   document.querySelector(".resetButton button").addEventListener('click', resetEverything)
 
 
-// CHANGE BACKGROUND UPON SCORE INCREASE CLICK-----------------------------
+// CHANGE BACKGROUND UPON SCORE INCREASE CLICK-SLU Colors-----------------------------
 const getRandomColor = (score) => {
   let color
   if (score % 2 === 0){
